00f01e11968eed5b5f4123acd8e2b65b0fa7006c
RibbitsM <rowan.murphy9@gmail.com>
2025-03-13 23:38:41 -0700

    fixed healthcare.sql

--- SQL/healthcare.sql
+++ SQL/healthcare.sql
@@ -6,7 +6,7 @@ CREATE TABLE provincial_governments (
     year INT,
     province VARCHAR(50),
     party VARCHAR(50),
-    PRIMARY KEY (year, province),
+    PRIMARY KEY (year, province)
 );
 
 CREATE TABLE percent_expenditure_change (
@@ -28,7 +28,7 @@ CREATE TABLE survey_expenditure (
     unmet_needs FLOAT(5),
     total_spend FLOAT(10) NOT NULL,
     year INT NOT NULL,
-    PRIMARY KEY (province),
+    PRIMARY KEY (province)
 );
 
 
@@ -635,31 +635,31 @@ values('2024', 'Quebec', '3.4');
 
 
 insert into survey_expenditure 
-values('0', 'Alberta', '5317.51', '2.682', '3.004', '1.096', '1.127', NULL, '23301.65', '2019');
+values('Alberta', '5317.51', '2.682', '3.004', '1.096', '1.127', NULL, '23301.65', '2019');
 
 insert into survey_expenditure 
-values('1', 'BritishColumbia', '4762.885', '2.832', '3.243', '1.105', '1.151', '1.945', '24506.35', '2019');
+values('BritishColumbia', '4762.885', '2.832', '3.243', '1.105', '1.151', '1.945', '24506.35', '2019');
 
 insert into survey_expenditure 
-values('2', 'Man.', '5021.065', '2.598', '3.24', '1.103', '1.112', '1.966', '6905.35', '2019');
+values('Man.', '5021.065', '2.598', '3.24', '1.103', '1.112', '1.966', '6905.35', '2019');
 
 insert into survey_expenditure 
-values('3', 'N.B.', '4593.585', '2.316', '3.931', '1.074', '1.072', '1.962', '3585.1', '2019');
+values('N.B.', '4593.585', '2.316', '3.931', '1.074', '1.072', '1.962', '3585.1', '2019');
 
 insert into survey_expenditure 
-values('4', 'N.L.', '6319.605', '2.293', '3.606', '1.051', '1.105', '1.965', '3332.1', '2019');
+values('N.L.', '6319.605', '2.293', '3.606', '1.051', '1.105', '1.965', '3332.1', '2019');
 
 insert into survey_expenditure 
-values('5', 'N.S.', '5121.23', '2.5', '4.011', '1.092', '1.113', '1.935', '5032.8', '2019');
+values('N.S.', '5121.23', '2.5', '4.011', '1.092', '1.113', '1.935', '5032.8', '2019');
 
 insert into survey_expenditure 
-values('6', 'Ontario', '4610.65', '2.65', '3.164', '1.073', '1.087', '1.952', '67652.75', '2019');
+values('Ontario', '4610.65', '2.65', '3.164', '1.073', '1.087', '1.952', '67652.75', '2019');
 
 insert into survey_expenditure 
-values('7', 'P.E.I.', '5101.495', '2.271', '3.336', '1.071', '1.09', NULL, '803.55', '2019');
+values('P.E.I.', '5101.495', '2.271', '3.336', '1.071', '1.09', NULL, '803.55', '2019');
 
 insert into survey_expenditure 
-values('8', 'Quebec', '5037.435', '2.771', '3.809', '1.151', '1.17', '1.953', '42920.3', '2019');
+values('Quebec', '5037.435', '2.771', '3.809', '1.151', '1.17', '1.953', '42920.3', '2019');
 
 insert into survey_expenditure 
-values('9', 'Sask.', '5198.73', '2.542', '2.977', '1.093', '1.125', NULL, '6060.95', '2019');
+values('Sask.', '5198.73', '2.542', '2.977', '1.093', '1.125', NULL, '6060.95', '2019');
